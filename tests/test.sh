197ece31d3783100afade70421590bd1d35739da
typeHaller <halle@remash.com>
2018-03-14 22:27:54 -0500

    Updated testing for Testing the Lexer and Parser Pipeline

--- tests/test.sh
+++ tests/test.sh
@@ -1,56 +1,22 @@
 #!/bin/bash
 
-# Citation: https://www.unr.edu/it/research-resources/the-grid/using-the-grid/bash-commands 
+./compiler ../tests/test1.src                            > ../tests/test1.result 
 
-# Fatal error: exception Failure("Unexpected end of file encountered")                  Do I want this behavior?
-./compiler ../tests/testBoolOnly                            &> ../tests/OutputResult
-./compiler ../tests/testIfBooleanFalse                      >>   ../tests/OutputResult
-./compiler ../tests/testIfBooleanOutput                     >>   ../tests/OutputResult
-./compiler ../tests/testIfBooleanTrue                       >>   ../tests/OutputResult
-./compiler ../tests/testIfLeqFalse                          >>   ../tests/OutputResult
-./compiler ../tests/testIfLeqTrue                           >>   ../tests/OutputResult
-# Fatal error: exception Failure("Unexpected Value Provided: Not a VBool")
-./compiler ../tests/testIfNumericCondition                  &>>  ../tests/OutputResult
-./compiler ../tests/testIntAdd                              >>   ../tests/OutputResult                  
-# Fatal error: exception Failure("Unexpected token found: false")
-./compiler ../tests/testIntAddBoolean                       &>>  ../tests/OutputResult                  
-# Fatal error: exception Failure("Unexpected token found: true")
-./compiler ../tests/testIntAddBooleans                      &>>  ../tests/OutputResult
-./compiler ../tests/testIntDivide                           >>   ../tests/OutputResult
-./compiler ../tests/testIntDivideByNestedZero               &>>  ../tests/OutputResult
-# Prints: Fatal error: exception Failure("Dividing by zero is bad")
-./compiler ../tests/testIntDivideByZero                     &>>  ../tests/OutputResult
-# Fatal error: exception Failure("Dividing by zero is bad")
-./compiler ../tests/testIntLeqFalse                         >>   ../tests/OutputResult
-./compiler ../tests/testIntLeqNestedNegativeTrue            >>   ../tests/OutputResult
-./compiler ../tests/testIntLeqNestedTrue                    >>   ../tests/OutputResult
-# Fatal error: exception Failure("Unexpected token found: true")
-./compiler ../tests/testIntLeqNonNumericInput               &>>  ../tests/OutputResult
-./compiler ../tests/testIntLeqTrue                          >>   ../tests/OutputResult
-./compiler ../tests/testIntLeqTrueEqual                     >>   ../tests/OutputResult
-./compiler ../tests/testIntMultiply                         >>   ../tests/OutputResult
-./compiler ../tests/testIntNegativeNestedOps                >>   ../tests/OutputResult
-./compiler ../tests/testIntNegativeResultAdd                >>   ../tests/OutputResult
-./compiler ../tests/testIntNegativeResultDivide             >>   ../tests/OutputResult
-./compiler ../tests/testIntNegativeResultMulitplication     >>   ../tests/OutputResult
-./compiler ../tests/testIntNegativeResultSubtraction        >>   ../tests/OutputResult
-./compiler ../tests/testIntNestedOps                        >>   ../tests/OutputResult
-# Fatal error: exception Failure("Unexpected end of file encountered")                  Do I want this behavior?
-./compiler ../tests/testIntOnly                             &>> ../tests/OutputResult
-./compiler ../tests/testIntSubtract                         >>  ../tests/OutputResult
-./compiler ../tests/testLParenMismatch                      &>> ../tests/OutputResult
-# Fatal error: exception Failure("Unexpected end of file encountered")
-./compiler ../tests/testNegativeInput                       &>> ../tests/OutputResult
-# Fatal error: exception Failure("Unexpected token found: -")
-./compiler ../tests/testRParenMismatch                      &>> ../tests/OutputResult
-# Fatal error: exception Failure("Unexpected token found: +")
-./compiler ../tests/testUnknownOperator                     &>> ../tests/OutputResult
-# Fatal error: exception Failure("Unexpected character found: %")
+ DIFF=$(diff ../tests/test1.out ../tests/test1.result )
+  if [ "$DIFF" != "" ]; then 
+    exit -1
+  fi
 
-#Citation: https://stackoverflow.com/questions/3611846/bash-using-the-result-of-a-diff-in-a-if-statement
-#Citation: https://unix.stackexchange.com/questions/81998/understanding-of-diff-output
+./compiler -lex ../tests/test1.src                       > ../tests/test1.lex.result
 
- DIFF=$(diff ../tests/OutputResult ../tests/ExpectedResult)
+ DIFF=$(diff ../tests/test1.lex.out ../tests/test1.lex.result )
+  if [ "$DIFF" != "" ]; then 
+    exit -1
+  fi
+
+./compiler -parse ../tests/test1.src                     > ../tests/test1.parse.result
+
+ DIFF=$(diff ../tests/test1.parse.out ../tests/test1.parse.result )
   if [ "$DIFF" != "" ]; then 
     exit -1
   fi
