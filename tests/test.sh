7b48c94b2e2b162cc473da7f2cddd380efe3150d
typeHaller <halle@remash.com>
2018-03-14 22:52:44 -0500

    Added Tests

--- tests/test.sh
+++ tests/test.sh
@@ -20,3 +20,45 @@
   if [ "$DIFF" != "" ]; then 
     exit -1
   fi
+
+./compiler ../tests/test2.src                            > ../tests/test2.result 
+
+ DIFF=$(diff ../tests/test2.out ../tests/test2.result )
+  if [ "$DIFF" != "" ]; then 
+    exit -1
+  fi
+
+./compiler -lex ../tests/test2.src                       > ../tests/test2.lex.result
+
+ DIFF=$(diff ../tests/test2.lex.out ../tests/test2.lex.result )
+  if [ "$DIFF" != "" ]; then 
+    exit -1
+  fi
+
+./compiler -parse ../tests/test2.src                     > ../tests/test2.parse.result
+
+ DIFF=$(diff ../tests/test2.parse.out ../tests/test2.parse.result )
+  if [ "$DIFF" != "" ]; then 
+    exit -1
+  fi
+
+./compiler ../tests/test3.src                            > ../tests/test3.result 
+
+ DIFF=$(diff ../tests/test3.out ../tests/test3.result )
+  if [ "$DIFF" != "" ]; then 
+    exit -1
+  fi
+
+./compiler -lex ../tests/test3.src                       > ../tests/test3.lex.result
+
+ DIFF=$(diff ../tests/test3.lex.out ../tests/test3.lex.result )
+  if [ "$DIFF" != "" ]; then 
+    exit -1
+  fi
+
+./compiler -parse ../tests/test3.src                     > ../tests/test3.parse.result
+
+ DIFF=$(diff ../tests/test3.parse.out ../tests/test3.parse.result )
+  if [ "$DIFF" != "" ]; then 
+    exit -1
+  fi
